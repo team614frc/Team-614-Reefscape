760060091fc063fd31023f406a90759a95fc3a84
Shiv-Shah <shivshah12698@gmail.com>
2025-01-05 11:13:44 -0500

    fix: update libs to fix build failure

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "36cb1d0d8bfda84603b00f28dd8df3785fe93608";
-  public static final String GIT_DATE = "2025-01-04 20:25:39 EST";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "131f13ae1995cb6e7f2fdcd81fa0894ef4231a27";
+  public static final String GIT_DATE = "2025-01-05 11:05:56 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-05 10:57:59 EST";
-  public static final long BUILD_UNIX_TIME = 1736092679446L;
+  public static final String BUILD_DATE = "2025-01-05 11:13:00 EST";
+  public static final long BUILD_UNIX_TIME = 1736093580080L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
