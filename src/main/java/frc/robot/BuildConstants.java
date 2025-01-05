f29407c3b18671a3ec9753936f821a4b34dad359
Shiv-Shah <shivshah12698@gmail.com>
2025-01-04 20:24:09 -0500

    This is the inital comit and I've added a build and i've added build info to know the build that is always running.

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Team-614-Reefscape";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "6ad3ef508c1d47403b307554031cb7b51799393f";
+  public static final String GIT_DATE = "2025-01-03 22:28:21 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-04 20:21:54 EST";
+  public static final long BUILD_UNIX_TIME = 1736040114478L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
