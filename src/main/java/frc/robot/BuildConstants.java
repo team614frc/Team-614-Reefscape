85f87b47c50a5739c81e40ab93779ac3672132a0
minhd-vu <minhd_vu@yahoo.com>
2025-01-10 17:27:14 -0500

    fix: build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Team614-Reefscape";
+  public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "b424dbb7d70717a2e52708b9fdd23a6153640af6";
-  public static final String GIT_DATE = "2025-01-05 11:13:44 EST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "53d3c0591c4b2fbae74d9049adafc2da309aaca1";
+  public static final String GIT_DATE = "2025-01-10 16:34:51 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-07 17:13:47 EST";
-  public static final long BUILD_UNIX_TIME = 1736288027374L;
+  public static final String BUILD_DATE = "2025-01-10 17:24:10 EST";
+  public static final long BUILD_UNIX_TIME = 1736547850635L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
