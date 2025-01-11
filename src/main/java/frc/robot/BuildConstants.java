684b6bfaf9c43f00dd1a49c2d9c9ba1a863c535f
minhd-vu <minhd_vu@yahoo.com>
2025-01-11 11:40:27 -0500

    chore: update build constants

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "53d3c0591c4b2fbae74d9049adafc2da309aaca1";
-  public static final String GIT_DATE = "2025-01-10 16:34:51 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "85f87b47c50a5739c81e40ab93779ac3672132a0";
+  public static final String GIT_DATE = "2025-01-10 17:27:14 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-10 17:24:10 EST";
-  public static final long BUILD_UNIX_TIME = 1736547850635L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-11 11:36:35 EST";
+  public static final long BUILD_UNIX_TIME = 1736613395499L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
