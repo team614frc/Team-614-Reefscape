57c5ab3e8654d712945a2b6a7a7ca44e6f4fa76d
Aashima Saxena <saxena.aashima.01@gmail.com>
2025-01-25 11:29:55 -0500

    chore: update WPIlib to 2025.2.1 (#7)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,12 +6,12 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "bea3dc806aa6de66c37340da46e423af893c4885";
-  public static final String GIT_DATE = "2025-01-18 09:24:47 EST";
-  public static final String GIT_BRANCH = "lucas/endeffector";
-  public static final String BUILD_DATE = "2025-01-18 10:17:57 EST";
-  public static final long BUILD_UNIX_TIME = 1737213477248L;
-  public static final int DIRTY = 0;
+  public static final String GIT_SHA = "6a3ea604e6acfe835b669f91f6d053f6259518b5";
+  public static final String GIT_DATE = "2025-01-18 23:06:18 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-25 09:21:12 EST";
+  public static final long BUILD_UNIX_TIME = 1737814872991L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
