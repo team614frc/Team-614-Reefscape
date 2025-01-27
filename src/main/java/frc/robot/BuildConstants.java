c4e16975670d079331fe16305d0bc9837eba2766
JetBluefan1 <jetbluefan01@gmail.com>
2025-01-27 11:30:30 -0500

    fix: swerve pid

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "a900faf4c5f9eddf2719185dd0711b8224b3a716";
-  public static final String GIT_DATE = "2025-01-11 13:16:05 EST";
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "7bab9a5b335306884f7ee285eb878ba26eaf8105";
+  public static final String GIT_DATE = "2025-01-14 15:33:32 EST";
   public static final String GIT_BRANCH = "xavier/elevator";
-  public static final String BUILD_DATE = "2025-01-14 11:38:22 EST";
-  public static final long BUILD_UNIX_TIME = 1736872702820L;
+  public static final String BUILD_DATE = "2025-01-27 11:22:43 EST";
+  public static final long BUILD_UNIX_TIME = 1737994963448L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
