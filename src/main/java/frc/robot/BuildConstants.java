f01c0993c01c3f449688521f99ab4b24b875be69
JetBluefan1 <jetbluefan01@gmail.com>
2025-02-01 10:31:02 -0500

    chore: spotless

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "57c5ab3e8654d712945a2b6a7a7ca44e6f4fa76d";
-  public static final String GIT_DATE = "2025-01-25 11:29:55 EST";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "2a8b71970cb1bad1777803a3f2423bc7c7e82829";
+  public static final String GIT_DATE = "2025-02-01 10:30:29 EST";
   public static final String GIT_BRANCH = "xavier/intake";
-  public static final String BUILD_DATE = "2025-02-01 10:26:26 EST";
-  public static final long BUILD_UNIX_TIME = 1738423586690L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-01 10:30:56 EST";
+  public static final long BUILD_UNIX_TIME = 1738423856816L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
