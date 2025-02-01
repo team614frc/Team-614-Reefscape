14355c1075586239a7ee3a975876a929ae57bb15
Lucas Nguyen <duyanlucasnguyen@gmail.com>
2025-02-01 09:16:07 -0500

    feat: Unit Testing End Effector Changes

--- src/test/java/EndEffectorTest.java
+++ src/test/java/EndEffectorTest.java
@@ -1,16 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
-import com.revrobotics.CANSparkFlex;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.sim.SparkFlexSim;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.hal.HAL;
+import edu.wpi.first.math.system.plant.DCMotor;
 import frc.robot.Constants;
-import frc.robot.Constants.EndEffectorConstants;
 import frc.robot.subsystems.EndEffectorSubsystem;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,36 +14,39 @@ import org.junit.jupiter.api.Test;
 class EndEffectorTest {
   static final double DELTA = 1e-2; // acceptable deviation range
   private EndEffectorSubsystem endEffector;
-  private CANSparkFlex mockMotor;
+  public SparkFlexSim m_simMotor;
 
   @BeforeEach // this method will run before each test
   void setup() {
     assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
-    mockMotor = mock(SparkFlex.class); // Create a mocked SparkFlex motor
-    endEffector = new EndEffectorSubsystem(mockMotor); // Inject mocked motor into subsystem
-    motorSim = new PWMSim(Constants.EndEffectorConstants.END_EFFECTOR_MOTOR); // Simulate motor
+    endEffector = new EndEffectorSubsystem(); // Inject mocked motor into subsystem
+
+    m_simMotor =
+        new SparkFlexSim(
+            new SparkFlex(Constants.EndEffectorConstants.END_EFFECTOR_MOTOR, MotorType.kBrushless),
+            new DCMotor(12.0, 2.22, 100, 120, 0, 1)); // Simulate motor
   }
 
   @AfterEach // this method will run after each test
   void shutdown() throws Exception {
-    endEffector.close(); // destroy our End Effector object
+    endEffector.set(0); // destroy our end effector object
   }
 
   @Test // marks this method as a test
-  void intake() {
-    endEffector.intakeTest(); // close the intake
-    assertEquals(EndEffectorConstants.INTAKE_SPEED, simMotor.getSpeed(), DELTA); // make sure that the value set to the motor is 0
+  void intakeTest() {
+    endEffector.intake().schedule(); // close the intake
+    assertEquals(Constants.EndEffectorConstants.INTAKE_SPEED, m_simMotor.getVelocity(), DELTA);
   }
 
   @Test
-  void outtake() {
-    endEffector.outtakeTest();
-    assertEquals(EndEffectorConstants.OUTTAKE_SPEED, simMotor.getSpeed(), DELTA);
+  void outtakeTest() {
+    endEffector.outtake().schedule();
+    assertEquals(Constants.EndEffectorConstants.OUTTAKE_SPEED, m_simMotor.getVelocity(), DELTA);
   }
 
   @Test
   void stopTest() {
     endEffector.stop();
-    assertEquals(0.0, motorSim.getSpeed(), DELTA);
+    assertEquals(0.0, m_simMotor.getVelocity(), DELTA);
   }
 }
