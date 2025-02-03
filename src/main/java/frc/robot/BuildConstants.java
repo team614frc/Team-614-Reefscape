39f3cf722288fae92e3dab4c4d12e45a01916a4f
lpd20 <lpd2007wb@gmail.com>
2025-02-03 16:54:13 -0500

    chore: updated vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "960542de2d8e54182b7eace3ea64a3ac52fa771b";
-  public static final String GIT_DATE = "2025-02-01 11:32:50 EST";
-  public static final String GIT_BRANCH = "xavier/intake";
-  public static final String BUILD_DATE = "2025-02-01 11:38:29 EST";
-  public static final long BUILD_UNIX_TIME = 1738427909712L;
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "ff83d45c1d9e22bb018608a17e3a7597a1c50505";
+  public static final String GIT_DATE = "2025-02-03 15:32:26 EST";
+  public static final String GIT_BRANCH = "lance/leds";
+  public static final String BUILD_DATE = "2025-02-03 16:14:59 EST";
+  public static final long BUILD_UNIX_TIME = 1738617299874L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
