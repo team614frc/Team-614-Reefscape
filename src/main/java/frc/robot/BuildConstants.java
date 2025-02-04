ed2126e28562117efc1854eddc024b3bde2f4c46
JetBluefan1 <70587680+JetBluefan1@users.noreply.github.com>
2025-02-04 08:11:41 -0500

    feat: elevatorarm (#14)
    * feat: elevatorarm
    
    * fix: constants
    
    * chore: comments
    
    * fix: comments
    
    * chore: comments and constants
    
    * chore: lib updates
    
    * fix: setpoints in bindings
    
    * fix: unneeded code

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "8135da217b49d196c2bc069dfc754d5584bc1f01";
-  public static final String GIT_DATE = "2025-02-01 11:46:33 EST";
-  public static final String GIT_BRANCH = "xavier/swerve";
-  public static final String BUILD_DATE = "2025-02-03 08:05:32 EST";
-  public static final long BUILD_UNIX_TIME = 1738587932020L;
+  public static final int GIT_REVISION = 18;
+  public static final String GIT_SHA = "a81f4eb017e2765987cfa80b23f3e7d0748804d3";
+  public static final String GIT_DATE = "2025-02-03 17:08:07 EST";
+  public static final String GIT_BRANCH = "xavier/elevatorarm";
+  public static final String BUILD_DATE = "2025-02-04 08:07:35 EST";
+  public static final long BUILD_UNIX_TIME = 1738674455618L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
