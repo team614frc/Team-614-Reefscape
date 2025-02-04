3d6af595af8c617b181ca3b1484057ce9cd554db
Lance <60494508+RealSirLancelot@users.noreply.github.com>
2025-02-04 15:48:26 -0500

    feat: added LEDs (#16)
    * feat: created LEDsSubsystem
    
    * feat: added led pattern commands
    
    * chore: formatting
    
    * chore: updated vendordeps
    
    * Update src/main/java/frc/robot/subsystems/LEDSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * chore: cleaned up code
    
    ---------
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "a81f4eb017e2765987cfa80b23f3e7d0748804d3";
-  public static final String GIT_DATE = "2025-02-03 17:08:07 EST";
-  public static final String GIT_BRANCH = "xavier/elevatorarm";
-  public static final String BUILD_DATE = "2025-02-04 08:07:35 EST";
-  public static final long BUILD_UNIX_TIME = 1738674455618L;
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "ff83d45c1d9e22bb018608a17e3a7597a1c50505";
+  public static final String GIT_DATE = "2025-02-03 15:32:26 EST";
+  public static final String GIT_BRANCH = "lance/leds";
+  public static final String BUILD_DATE = "2025-02-03 16:14:59 EST";
+  public static final long BUILD_UNIX_TIME = 1738617299874L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
