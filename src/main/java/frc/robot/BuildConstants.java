352093dcb783856f567b55b62adadb71ea028134
Lance <60494508+RealSirLancelot@users.noreply.github.com>
2025-02-07 17:28:24 -0500

    feat: add limelight (#4)
    * Update ci.yml
    
    This is added to check spotless as well
    
    * Update ci.yml pt 2
    
    fixing the spotless check
    
    * Update ci.yml
    
    This is to make it a bit more on when to run it.
    
    * This is the inital comit and I've added a build
    and i've added build info to know the build that
    is always running.
    
    * Update to Gradle
    
    Added visual VM connection. https://docs.wpilib.org/en/stable/docs/software/advanced-gradlerio/profiling-with-visualvm.html
    
    Use that as your refrence
    
    * Updated Libs due to build failture
    
    * fix: lib updates
    
    * feat: created limelight subsystem
    
    * feat: started work on vision alignment
    
    * feat: added limelight based path driving
    
    * feat: added limelight based path finding
    
    * chore: format
    
    * chore: build stuff
    
    * fix: merge conflicts
    
    * chore: rename revlib file
    
    * chore: remove paths
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/RobotContainer.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/subsystems/LimelightSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * fix: fixed ids for april tag in switch
    
    * fix: removed issue related to smartdashboard limelight values not being read in
    
    * fix: updated vision estimating based on testing
    
    * fix: driveToReef now works as intended
    
    * fix: additional testing
    
    * fix: removed testing junk
    
    * feat: replaced visionconstants usage with fieldconstants
    
    * fix: now uses enum for direction, removed testing code
    
    * chore: formatting
    
    * chore: formatting
    
    * chore: vendor updates
    
    * chore: removed uneeded path
    
    * chore: fixed DIRECTION enum
    
    * chore: typo
    
    * chore: logic error
    
    * fix: updated variable naming conventions
    
    * fix: simplified logic
    
    * Update src/main/java/frc/robot/FieldConstants.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * fix: using switch for enums
    
    * chore: library stuff
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * Update src/main/java/frc/robot/subsystems/SwerveSubsystem.java
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * chore: format
    
    * chore: build stuff
    
    * chore: update settings.json
    
    * Update .vscode/settings.json
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * feat: using nearest apriltag instead of visible apriltag for driveReef
    
    * fix: removed unused methods
    
    * chore: moved variable decleration
    
    Co-authored-by: Minh Vu <minhd_vu@yahoo.com>
    
    * fix: restructured list stuff
    
    * fix: uses javadocs format for comments
    
    ---------
    
    Co-authored-by: Shivam Shah <shivshah12698@gmail.com>
    Co-authored-by: jetbluefan1 <jetbluefan01@gmail.com>
    Co-authored-by: minhd-vu <minhd_vu@yahoo.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 21;
-  public static final String GIT_SHA = "c6aac83f70b735be74b2c0b7b8e62757b406d5dc";
-  public static final String GIT_DATE = "2025-02-04 16:33:38 EST";
-  public static final String GIT_BRANCH = "xavier/canal";
-  public static final String BUILD_DATE = "2025-02-04 16:44:53 EST";
-  public static final long BUILD_UNIX_TIME = 1738705493756L;
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "4109fe644cb5d22ddaf5af8b2c0194944c606ef8";
+  public static final String GIT_DATE = "2025-02-06 21:58:54 EST";
+  public static final String GIT_BRANCH = "lance/limelight";
+  public static final String BUILD_DATE = "2025-02-07 14:57:49 EST";
+  public static final long BUILD_UNIX_TIME = 1738958269285L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
