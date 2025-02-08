8e2719643d5b81b9aff0014c6186c80d45dd8fb7
Lucas Nguyen <duyanlucasnguyen@gmail.com>
2025-02-08 08:43:00 -0500

    feat: Climber Unit Test

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "81c2ced619ac415902b143114093cb03b0420177";
-  public static final String GIT_DATE = "2025-02-07 15:28:09 EST";
-  public static final String GIT_BRANCH = "lucas/UnitTestCanal";
-  public static final String BUILD_DATE = "2025-02-07 16:47:25 EST";
-  public static final long BUILD_UNIX_TIME = 1738964845798L;
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "c91fc694cb1ba16444a1f40fca76fe00177504d0";
+  public static final String GIT_DATE = "2025-02-08 08:29:33 EST";
+  public static final String GIT_BRANCH = "lucas/ClimberUnitTest";
+  public static final String BUILD_DATE = "2025-02-08 08:42:42 EST";
+  public static final long BUILD_UNIX_TIME = 1739022162075L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/test/java/ClimbTest.java
+++ src/test/java/ClimbTest.java
@@ -4,51 +4,45 @@ import com.revrobotics.sim.SparkFlexSim;
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.math.system.plant.DCMotor;
 import frc.robot.Constants;
-import frc.robot.subsystems.EndEffectorSubsystem;
+import frc.robot.subsystems.ClimberSubsystem;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class EndEffectorTest {
+class CanalTest {
   static final double DELTA = 1e-2; // acceptable deviation range
-  private EndEffectorSubsystem endEffector;
+  private ClimberSubsystem climber;
   private SparkFlexSim simMotor;
 
-  public EndEffectorTest() {}
+  public CanalTest() {}
 
   @BeforeEach // this method will run before each test
   void setup() {
     assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
-    endEffector = new EndEffectorSubsystem(); // Inject mocked motor into subsystem
+    climber = new ClimberSubsystem(); // Inject mocked motor into subsystem
 
-    simMotor = new SparkFlexSim(endEffector.getMotor(), DCMotor.getNeoVortex(1)); // Simulate motor
+    simMotor = new SparkFlexSim(climber.getMotor(), DCMotor.getNeoVortex(1)); // Simulate motor
     simMotor.enable();
   }
 
   @AfterEach // this method will run after each test
   void shutdown() throws Exception {
     simMotor.disable();
-    endEffector.getMotor().close();
+    climber.getMotor().close();
   }
 
   @Test // marks this method as a test
-  void intakeTest() {
+  void climbTest() {
 
     // executing the command
-    endEffector.intake().execute();
+    climber.climb().execute();
     // When the command is executed we are getting the speed at the subsystem
-    assertEquals(Constants.EndEffectorConstants.INTAKE_SPEED, endEffector.getMotor().get());
+    assertEquals(Constants.ClimberConstants.CLIMB_SPEED, climber.getMotor().get());
   }
 
   @Test
-  void outtakeTest() {
-    endEffector.outtake().execute();
-    assertEquals(Constants.EndEffectorConstants.OUTTAKE_SPEED, endEffector.getMotor().get());
-  }
-
-  @Test
-  void stopTest() {
-    endEffector.stop().execute();
-    assertEquals(0.0, endEffector.getMotor().get());
+  void reverseClimbTest() {
+    climber.reverseClimb().execute();
+    assertEquals(Constants.ClimberConstants.REVERSE_CLIMB_SPEED, climber.getMotor().get());
   }
 }
