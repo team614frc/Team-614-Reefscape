d605e07b300b89b3decb437e1c0f411ce6948b6c
JetBluefan1 <jetbluefan01@gmail.com>
2025-02-08 08:18:36 -0500

    chore: lib updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Team-614-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "5c736de78bd9554ad54e698faeaa81abb4106750";
-  public static final String GIT_DATE = "2025-02-08 08:07:20 EST";
+  public static final int GIT_REVISION = 30;
+  public static final String GIT_SHA = "2e959a1cfd9878a65ab94ddf8ad325783751382d";
+  public static final String GIT_DATE = "2025-02-08 08:10:41 EST";
   public static final String GIT_BRANCH = "xavier/commands";
-  public static final String BUILD_DATE = "2025-02-08 08:10:06 EST";
-  public static final long BUILD_UNIX_TIME = 1739020206046L;
+  public static final String BUILD_DATE = "2025-02-08 08:18:11 EST";
+  public static final long BUILD_UNIX_TIME = 1739020691534L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
