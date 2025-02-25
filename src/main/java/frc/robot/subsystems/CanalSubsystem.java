7e81602fb4d8572daf7279bd98b9d61a89b14b96
lpd20 <lpd2007wb@gmail.com>
2025-02-25 15:19:29 -0500

    chore: formatting

--- src/main/java/frc/robot/subsystems/CanalSubsystem.java
+++ src/main/java/frc/robot/subsystems/CanalSubsystem.java
@@ -10,7 +10,6 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
