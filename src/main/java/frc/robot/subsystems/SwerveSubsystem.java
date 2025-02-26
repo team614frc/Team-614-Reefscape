0ace2f0536625dc74839b1ba4ad7ff9d6fe8973b
lpd20 <lpd2007wb@gmail.com>
2025-02-26 15:19:30 -0500

    fix: prepared for swerve discretization tuning

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -87,13 +87,14 @@ public class SwerveSubsystem extends SubsystemBase {
         false); // !SwerveDriveTelemetry.isSimulation); // Disables cosine compensation for
     // simulations since it causes discrepancies not seen in real life.
     swerveDrive.setAngularVelocityCompensation(
-        true, true,
+        true, false,
         0.1); // Correct for skew that gets worse as angular velocity increases. Start with a
     // coefficient of 0.1.
     swerveDrive.setModuleEncoderAutoSynchronize(
         false, 1); // Enable if you want to resynchronize your absolute encoders and motor encoders
     // periodically when they are not moving.
     setupPathPlanner();
+    swerveDrive.setChassisDiscretization(true,.02);
     limelight = new LimelightSubsystem();
   }
 
