7a9aa75ede5df5623ac18960e02cd01e31445081
jetbluefan1 <jetbluefan01@gmail.com>
2025-02-27 23:18:06 -0500

    fix: buttons, adjustments to commands

--- src/main/java/frc/robot/subsystems/IntakePivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakePivotSubsystem.java
@@ -52,6 +52,11 @@ public class IntakePivotSubsystem extends SubsystemBase {
     intakePivotEncoder.setPosition(0);
   }
 
+  public boolean reachedSetpoint() {
+    return Math.abs(intakePivotEncoder.getPosition() - pivotSetpoint)
+        <= IntakeConstants.PIVOT_TOLERANCE;
+  }
+
   private double getPivotAngleRadians() {
     return Units.rotationsToRadians(intakePivotEncoder.getPosition());
   }
@@ -96,10 +101,17 @@ public class IntakePivotSubsystem extends SubsystemBase {
         });
   }
 
-  public Command pivotAlgae() {
+  public Command pivotIntakeAlgae() {
     return Commands.runOnce(
         () -> {
-          pivotSetpoint = IntakeConstants.PIVOT_ALGAE;
+          pivotSetpoint = IntakeConstants.PIVOT_INTAKE_ALGAE;
+        });
+  }
+
+  public Command pivotOuttakeAlgae() {
+    return Commands.runOnce(
+        () -> {
+          pivotSetpoint = IntakeConstants.PIVOT_OUTTAKE_ALGAE;
         });
   }
 }
