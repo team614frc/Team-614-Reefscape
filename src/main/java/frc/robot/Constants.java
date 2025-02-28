2031875226c1d29e56ab3bc1529241d809db2ab8
lpd20 <lpd2007wb@gmail.com>
2025-02-27 19:12:28 -0500

    feat: pid tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -53,23 +53,23 @@ public final class Constants {
     public static final double INTAKE_REST_SPEED = 0;
     public static final double OUTTAKE_REST_SPEED = 0;
 
-    public static final double PIVOT_kP = 0.01;
+    public static final double PIVOT_kP = 0.6;
     public static final double PIVOT_kI = 0;
-    public static final double PIVOT_kD = 0;
+    public static final double PIVOT_kD = 0.01;
     public static final double PIVOT_kS = 0;
-    public static final double PIVOT_kG = 0.7;
+    public static final double PIVOT_kG = 0.5;
     public static final double PIVOT_kV = 0;
     public static final double PIVOT_kA = 0;
     public static final double GEAR_RATIO = 60;
-    public static final double PIVOT_UP = -0.2;
-    public static final double PIVOT_DOWN = -6.35;
-    public static final double PIVOT_ALGAE = -2.8;
+    public static final double PIVOT_UP = -0.9;
+    public static final double PIVOT_DOWN = -6.9;
+    public static final double PIVOT_ALGAE = -3.4;
     public static final Mass PIVOT_WEIGHT = Kilogram.of(9.55);
     public static final double PIVOT_MOTOR_SPEED = 0.1;
     public static final double PIVOT_REST_SPEED = 0;
-    public static final double PIVOT_MAX_VELOCITY = 6;
-    public static final double PIVOT_MAX_ACCELERATION = 10;
-    public static final double PIVOT_FEEDFORWARD_OFFSET = -6.16;
+    public static final double PIVOT_MAX_VELOCITY = 4;
+    public static final double PIVOT_MAX_ACCELERATION = 5;
+    public static final double PIVOT_FEEDFORWARD_OFFSET = -6.3;
   }
 
   public static class EndEffectorConstants {
