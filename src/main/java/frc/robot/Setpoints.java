28c50ada2f76c55425a7a26d9537157dc564d8a9
lpd20 <lpd2007wb@gmail.com>
2025-03-29 09:21:43 -0400

    fix: removed uneeded constants

--- src/main/java/frc/robot/Setpoints.java
+++ src/main/java/frc/robot/Setpoints.java
@@ -7,51 +7,6 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 
 public class Setpoints {
-
-  public static class Wrist {
-    public static final double rest = 0.27;
-    public static final double active = 0;
-  }
-
-  public static class Elevator {
-
-    public static class Coral {
-
-      public static double L1 = 0;
-      public static double L2 = 0.014;
-      public static double L3 = 0.014;
-      public static double L4 = 0.47;
-      public static double HP = 0;
-    }
-
-    public static class Algae {
-      public static final double L23 = 0.039;
-      public static final double L34 = 0.0566;
-      public static final double NET = 0.735;
-      public static final double PROCESSOR = 0.059;
-    }
-  }
-
-  public static class Arm {
-
-    public static class Coral {
-
-      public static double HP = 13; // TOtAl GuesS
-      public static double L1 = 0;
-      public static double L2 = 10;
-      public static double L3 = 45.14;
-      public static double L4 = 67.9;
-    }
-
-    public static class Algae {
-
-      public static final double L34 = 37.2;
-      public static final double L23 = 2.637;
-      public static final double NET = 90;
-      public static final double PROCESSOR = -32; // Guess
-    }
-  }
-
   public static class AutoScoring {
     public static class Processor {
       public static final Transform2d offset =
